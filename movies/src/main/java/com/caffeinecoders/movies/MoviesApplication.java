f7f8bf33f252f184b0fadd7dabe601788f79feda
savisaluwadana <82082138+savisaluwadana@users.noreply.github.com>
2025-10-17 08:39:57 +0530

    Add MongoDB dependencies and configuration, and create Movie entity class

--- movies/src/main/java/com/caffeinecoders/movies/MoviesApplication.java
+++ movies/src/main/java/com/caffeinecoders/movies/MoviesApplication.java
@@ -2,12 +2,24 @@ package com.caffeinecoders.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//@RestController
+
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
+//	@GetMapping("/")
+//	public String apiRoot() {
+//		return "Hello from Movies Service";
+//	}
+//	@GetMapping("/test1")
+//	public String test1() {
+//		return "Test1 from Movies Service";
+//	}
 
 }
---
+++ movies/src/test/java/com/caffeinecoders/movies/Movie.java
@@ -0,0 +1,21 @@
+package com.caffeinecoders.movies;
+
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+}
